5ff24e0b1bd5f6ceb8d148b01720125221a17744
NakaiNozomi <n.naka.23.1.769@4nonome.com>
2023-03-15 20:21:52 +0900

    サインアップ修正（５）

--- accounts/tests.py
+++ accounts/tests.py
@@ -44,7 +44,10 @@ class TestSignupView(TestCase):
 
         self.assertEqual(response.status_code, 200)
         self.assertEqual(User.objects.count(), 0)
-        self.assertIn("このフィールドは必須です", form.errors)
+        self.assertIn("このフィールドは必須です。", form.errors["username"])
+        self.assertIn("このフィールドは必須です。", form.errors["email"])
+        self.assertIn("このフィールドは必須です。", form.errors["password1"])
+        self.assertIn("このフィールドは必須です。", form.errors["password2"])
 
     def test_failure_post_with_empty_username(self):
         invalid_data = {
