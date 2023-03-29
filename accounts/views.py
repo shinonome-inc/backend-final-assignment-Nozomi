9188cbc3574f0b61c2f4e0b0beb72f9d42d12536
NakaiNozomi <n.naka.23.1.769@4nonome.com>
2023-03-29 15:39:35 +0900

    2次レビュー修正（1）

--- accounts/views.py
+++ accounts/views.py
@@ -1,17 +1,18 @@
-# from django.shortcuts import render
+from django.conf import settings
 from django.contrib.auth import authenticate, login
+from django.contrib.auth import views as auth_views
 from django.contrib.auth.mixins import LoginRequiredMixin
-from django.contrib.auth.views import LoginView, LogoutView
 from django.urls import reverse_lazy
-from django.views.generic import CreateView
+from django.views.generic import CreateView, DetailView
 
+from .admin import User
 from .forms import LoginForm, SignupForm
 
 
 class SignupView(CreateView):
     form_class = SignupForm
     template_name = "accounts/signup.html"
-    success_url = reverse_lazy("tweets:home")
+    success_url = reverse_lazy(settings.LOGIN_REDIRECT_URL)
 
     def form_valid(self, form):
         response = super().form_valid(form)
@@ -22,10 +23,17 @@ class SignupView(CreateView):
         return response
 
 
-class LoginView(LoginView):
+class LoginView(auth_views.LoginView):
     form_class = LoginForm
     template_name = "accounts/login.html"
 
 
-class LogoutView(LoginRequiredMixin, LogoutView):
+class LogoutView(LoginRequiredMixin, auth_views.LogoutView):
     template_name = "accounts/login.html"
+
+
+class UserProfileView(LoginRequiredMixin, DetailView):
+    model = User
+    template_name = "profile.html"
+    slug_field = "username"
+    slug_url_kwarg = "username"
