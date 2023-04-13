5672d5e4685017d8441006dbea673d0be5630180
NakaiNozomi <n.naka.23.1.769@4nonome.com>
2023-04-13 15:22:19 +0900

    Tweet機能修正(2)

--- tweets/views.py
+++ tweets/views.py
@@ -17,7 +17,7 @@ class HomeView(LoginRequiredMixin, ListView):
 
     def get_context_data(self, **kwargs):
         context = super().get_context_data(**kwargs)
-        context["tweet_list"] = Tweet.objects.select_related('user')
+        context["tweet_list"] = Tweet.objects.select_related("user")
         return context
 
 
